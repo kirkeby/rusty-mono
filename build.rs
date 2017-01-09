39ffe2a07a6940f9e2f049ad8ecbe490abe762e4
Sune Kirkeby <mig@ibofobi.dk>
2017-01-09 16:34:13 +0100

    Includes.

--- build.rs
+++ build.rs
@@ -1,10 +1,29 @@
 extern crate gcc;
 
+const INCLUDES : &'static [&'static str] = &[
+    "",
+    "mbed",
+    "mbed/api",
+    "mbed/hal",
+    "mbed/target_cypress",
+    "mbed/libraries/fs/sd",
+    "mbed/libraries/fs/fat",
+    "mbed/libraries/fs/fat/ChaN",
+    "display",
+    "display/ili9225g",
+    "display/ui",
+    "media",
+    "wireless",
+    "mbed/target_cypress",
+];
+
 fn main() {
-    gcc::Config::new()
-        .cpp(true)
-        .include("../mono_framework/src")
-        .file("src/stubs.cc")
-        .compile("libstubs.a");
+    let sdk_root = "../mono_framework/dist/mono/include/".to_string();
+    let mut gcc = gcc::Config::new();
+    gcc.cpp(true);
+    for i in INCLUDES {
+        gcc.include(format!("{}{}", sdk_root, i));
+    }
+    gcc.file("src/stubs.cc").compile("libstubs.a");
     println!("cargo:rustc-link-lib=static=empty");
 }
