a373977e08444f1a2b4e99109d38dd92426d5cde
Sune Kirkeby <mig@ibofobi.dk>
2017-01-11 11:26:49 +0100

    Add From for &str -> mono::String.

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,11 @@ mod generated;
 pub mod helpers;
 
 pub use generated::mono;
+
+impl From<&'static str> for mono::String {
+    fn from(s: &str) -> mono::String {
+        mono::String::new_Pcj(
+            s.as_ptr() as *mut i8,
+            s.len() as u32)
+    }
+}
