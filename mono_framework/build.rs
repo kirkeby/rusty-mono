e95a01686b4377b3b52218250591d3f4ed99abc5
Sune Kirkeby <mig@ibofobi.dk>
2017-01-18 15:08:12 +0100

    Remove superflous archive mangling.

--- mono_framework/build.rs
+++ mono_framework/build.rs
@@ -1,7 +1,5 @@
 extern crate gcc;
 
-use std::process::Command;
-
 const INCLUDES : &'static [&'static str] = &[
     "",
     "mbed",
@@ -30,11 +28,4 @@ fn main() {
     gcc.file("src/generated.cc");
     gcc.file("src/helpers.cc");
     gcc.compile("libgenerated.a");
-
-    // The Mono framework comes with a bunch of very broken ar(1) archives,
-    // first problem is they're not named properly, so cc(1) can not find them
-    // and link against them, second problem is that some of them contain
-    // non-object-files, which really make Rust unhappy.
-    Command::new("./build.sh").status().expect("build.sh failed");
-    println!("cargo:rustc-link-lib=static=all-of-mono");
 }
